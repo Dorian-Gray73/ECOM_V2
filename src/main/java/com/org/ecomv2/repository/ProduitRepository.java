89286212e9d1bbdacba2cf7326c0fb5db4f300b8
DorianGray <doriansimon73@gmail.com>
2022-09-13 22:22:00 +0200

    feat(Get Ray-ban): ajout dans le Repository

--- src/main/java/com/org/ecomv2/repository/ProduitRepository.java
+++ src/main/java/com/org/ecomv2/repository/ProduitRepository.java
@@ -3,10 +3,15 @@ package com.org.ecomv2.repository;
 import com.org.ecomv2.domain.Produit;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Spring Data JPA repository for the Produit entity.
  */
-@SuppressWarnings("unused")
+//@SuppressWarnings("unused")
 @Repository
-public interface ProduitRepository extends JpaRepository<Produit, Long> {}
+@Transactional
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    @Query("SELECT nom FROM Produit p WHERE p.marque='Rayban'")
+    String GetRayban();
+}
