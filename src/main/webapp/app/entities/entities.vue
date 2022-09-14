752f7486f0a913f233ea6a2e5a362af01b743ad2
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-14 11:34:18 +0200

    style(Admin) : fix navbar admin entities

--- src/main/webapp/app/entities/entities.vue
+++ src/main/webapp/app/entities/entities.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <ribbon></ribbon>
+    <jhi-navbar></jhi-navbar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts" src="./entities.component.ts"></script>
