29ed19278363e6e12d9a5b05beaee56f5640c94e
Jad <jad.jawlakh7@gmail.com>
2022-09-16 13:09:52 +0200

    feat(get-couleurs)

--- src/main/java/com/org/ecomv2/repository/CaracteristiqueRepository.java
+++ src/main/java/com/org/ecomv2/repository/CaracteristiqueRepository.java
@@ -1,12 +1,18 @@
 package com.org.ecomv2.repository;
 
 import com.org.ecomv2.domain.Caracteristique;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Spring Data JPA repository for the Caracteristique entity.
  */
-@SuppressWarnings("unused")
+
+@Transactional
 @Repository
-public interface CaracteristiqueRepository extends JpaRepository<Caracteristique, Long> {}
+public interface CaracteristiqueRepository extends JpaRepository<Caracteristique, Long> {
+    @Query("SELECT DISTINCT(c.couleur) FROM Caracteristique c")
+    List<String> getAllCouleurs();
+}
