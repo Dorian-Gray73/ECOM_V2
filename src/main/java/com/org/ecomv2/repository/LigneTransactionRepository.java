d2d7bd8e3207826e8a314ad773ed5c5c61f0c93b
Jad <jad.jawlakh7@gmail.com>
2022-09-17 11:28:44 +0200

    feat(update-ligne-transaction-quantite)

--- src/main/java/com/org/ecomv2/repository/LigneTransactionRepository.java
+++ src/main/java/com/org/ecomv2/repository/LigneTransactionRepository.java
@@ -2,11 +2,20 @@ package com.org.ecomv2.repository;
 
 import com.org.ecomv2.domain.LigneTransaction;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Spring Data JPA repository for the LigneTransaction entity.
  */
-@SuppressWarnings("unused")
+@Transactional
 @Repository
-public interface LigneTransactionRepository extends JpaRepository<LigneTransaction, Long> {}
+public interface LigneTransactionRepository extends JpaRepository<LigneTransaction, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE LigneTransaction SET quantite = :newQuantite WHERE id = :ligneTransactionId")
+    public void updateLigneTransactionQuantite(
+        @Param("ligneTransactionId") Long ligneTransactionId,
+        @Param("newQuantite") Integer newQuantite
+    );
+}
