59ba8dc852990b2744699fe614beff911c53712f
Damien Lombard <damien.lombard@etu.univ-grenoble-alpes.fr>
2022-09-19 16:38:34 +0200

    style (authentification) : suppression de dossier dossier de travail contenant des copies de fichiers en cours de modification.

--- src/main/webapp/app/account/registerbis/registerbis.service.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default class RegisterBisService {
-  public processRegistration(account: any): Promise<any> {
-    return axios.post('api/register', account);
-  }
-}
