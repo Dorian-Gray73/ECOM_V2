c1fd6dc11728ce1a7422e4085d086dbc0f4390da
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-21 10:39:47 +0200

    style(LoginApp) : connexion et page admin

---
+++ src/main/webapp/app/core/login-app/login-app.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject, Vue } from 'vue-property-decorator';
+import LoginService from '@/account/login.service';
+import LoginForm from '@/account/login-form/login-form.vue';
+import AccountService from '@/account/account.service';
+import axios from 'axios';
+
+@Component({
+  components: {
+    'login-form': LoginForm,
+  },
+})
+export default class LoginApp extends Vue {
+  @Inject('accountService')
+  private accountService: () => AccountService;
+  public authenticationError = null;
+  public login = null;
+  public password = null;
+  public rememberMe: boolean = null;
+
+  public doLogin(): void {
+    const data = { username: this.login, password: this.password, rememberMe: this.rememberMe };
+    axios
+      .post('api/authenticate', data)
+      .then(result => {
+        const bearerToken = result.headers.authorization;
+        if (bearerToken && bearerToken.slice(0, 7) === 'Bearer ') {
+          const jwt = bearerToken.slice(7, bearerToken.length);
+          if (this.rememberMe) {
+            localStorage.setItem('jhi-authenticationToken', jwt);
+            sessionStorage.removeItem('jhi-authenticationToken');
+          } else {
+            sessionStorage.setItem('jhi-authenticationToken', jwt);
+            localStorage.removeItem('jhi-authenticationToken');
+          }
+        }
+        this.authenticationError = false;
+        this.accountService().retrieveAccount();
+        this.$router.push({ name: 'Catalogue' });
+      })
+      .catch(() => {
+        this.authenticationError = true;
+      });
+  }
+}
---
+++ src/main/webapp/app/core/login-app/login-app.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="loginApp">
+    <span class="title">À propos de nous</span>
+    <div id="divBtnConnexion">
+      <!-- <b-button v-on:click="seConnecter()" class="btnConnexion">S'enregistrer</b-button>
+      <b-button v-on:click="openLogin()" class="btnConnexion">Se connecter</b-button>-->
+      <div class="modal-body">
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+            <b-alert show data-cy="loginError" variant="danger" v-if="authenticationError">
+              <strong>Erreur lors de la connexion</strong> Merci de vérifier vos informations et de réessayer.
+            </b-alert>
+          </div>
+          <div class="col-md-8">
+            <b-form role="form" v-on:submit.prevent="doLogin()">
+              <b-form-group label="Identifiant" label-for="username">
+                <b-form-input
+                  id="username"
+                  type="text"
+                  name="username"
+                  autofocus
+                  placeholder="Votre identifiant"
+                  v-model="login"
+                  data-cy="username"
+                >
+                </b-form-input>
+              </b-form-group>
+              <b-form-group label="Mot de passe" label-for="password">
+                <b-form-input
+                  id="password"
+                  type="password"
+                  name="password"
+                  v-model.trim="name"
+                  placeholder="Votre mot de passe"
+                  v-model="password"
+                  data-cy="password"
+                >
+                </b-form-input>
+              </b-form-group>
+              <b-form-checkbox id="rememberMe" name="rememberMe" v-model="rememberMe" checked>
+                <span>Se souvenir de moi</span>
+              </b-form-checkbox>
+              <div>
+                <b-button data-cy="submit" type="submit" variant="primary">Se connecter</b-button>
+              </div>
+            </b-form>
+            <p></p>
+            <div>
+              <b-alert show variant="warning">
+                <span>Vous n'avez pas encore de compte ?</span>
+                <b-link :to="'/register'" class="alert-link">Enregistrer un compte</b-link>
+              </b-alert>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" src="./login-app.component.ts"></script>
+<style scoped>
+#divBtnConnexion {
+  margin: auto;
+}
+</style>
