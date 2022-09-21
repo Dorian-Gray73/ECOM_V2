7be6282a70a2cf5d681b98ffa094847c2d218cbc
Jad <jad.jawlakh7@gmail.com>
2022-09-21 08:57:19 +0200

    fix(restauration code écrasé par generation champ jhipster)

--- src/main/java/com/org/ecomv2/repository/CaracteristiqueRepository.java
+++ src/main/java/com/org/ecomv2/repository/CaracteristiqueRepository.java
@@ -2,6 +2,7 @@ package com.org.ecomv2.repository;
 
 import com.org.ecomv2.domain.Caracteristique;
 import java.util.List;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,11 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository
 public interface CaracteristiqueRepository extends JpaRepository<Caracteristique, Long> {
-
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
     @Query(value = "SELECT * FROM Caracteristique c JOIN Produit p ON c.produit_id = p.id WHERE p.id = :produitId", nativeQuery = true)
     List<Caracteristique> getCaracteristiquesBy(@Param("produitId") long produitId);
 
     @Query("SELECT DISTINCT(c.couleur) FROM Caracteristique c")
     List<String> getAllCouleurs();
-
 }
