95c31a09494db0f5a970a1f330361b5189db796c
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-21 13:10:13 +0200

    style(Paiement) : var to let

--- src/main/webapp/app/core/paiement/paiment.component.ts
+++ src/main/webapp/app/core/paiement/paiment.component.ts
@@ -22,12 +22,12 @@ export default class Paiement extends Vue {
   }
 
   public formatCardNumber(value) {
-    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
-    var matches = v.match(/\d{4,16}/g);
-    var match = (matches && matches[0]) || '';
-    var parts = [];
+    let v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+    let matches = v.match(/\d{4,16}/g);
+    let match = (matches && matches[0]) || '';
+    let parts = [];
 
-    for (var i = 0; i < match.length; i += 4) {
+    for (let i = 0; i < match.length; i += 4) {
       parts.push(match.substring(i, i + 4));
     }
 
