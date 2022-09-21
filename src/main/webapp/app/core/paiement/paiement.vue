54681ea9d4377297efdcad655bc182d0e87a624b
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-21 11:38:20 +0200

    style(Paiement) : formulaire de paiement

--- src/main/webapp/app/core/paiement/paiement.vue
+++ src/main/webapp/app/core/paiement/paiement.vue
@@ -8,7 +8,34 @@
       <div class="step">Confirmation</div>
     </div>
     <div id="contenuPanier">
-      <b-button v-on:click="validerPaiement()">S'enregistrer</b-button>
+      <div id="formulaire">
+        <b-form-input
+          id="input-1"
+          class="formu"
+          v-model="cardNumber"
+          placeholder="Numéro de carte"
+          :formatter="formatCardNumber"
+          required
+        />
+        <b-form-input id="input-2" class="formu" v-model="cardName" placeholder="Nom de la carte" required />
+        <div class="card-form__group formu">
+          <label for="cardMonth" class="card-input__label">Date d'expiration</label>
+          <select class="card-input__input -select" id="cardMonth" v-model="cardMonth" data-ref="cardDate">
+            <option value="" disabled selected>Mois</option>
+            <option v-bind:value="n < 10 ? '0' + n : n" v-for="n in 12" v-bind:disabled="n < minCardMonth" v-bind:key="n">
+              {{ n < 10 ? '0' + n : n }}
+            </option>
+          </select>
+          <select class="card-input__input -select" id="cardYear" v-model="cardYear" data-ref="cardDate">
+            <option value="" disabled selected>Année</option>
+            <option v-bind:value="$index + minCardYear" v-for="(n, $index) in 12" v-bind:key="n">
+              {{ $index + minCardYear }}
+            </option>
+          </select>
+        </div>
+        <b-form-input id="input-3" class="formu" v-model="cardCvv" maxlength="4" placeholder="CVV" required />
+        <b-button class="formu" variant="primary">Valider</b-button>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +46,6 @@
   margin: 20px 128px;
   gap: 32px;
   background-color: #ffffff;
-  height: 128px;
 }
 
 .progressionPanier {
@@ -45,5 +71,12 @@
   font-weight: bold;
   border-bottom: solid 1px #5b85aa;
   padding-bottom: 7px;
+}
+
+#formulaire {
+  display: flex;
+  flex-direction: column;
+  margin: 32px auto;
+  gap: 32px;
 }
 </style>
--- src/main/webapp/app/core/paiement/paiment.component.ts
+++ src/main/webapp/app/core/paiement/paiment.component.ts
@@ -1,8 +1,40 @@
 import { Component, Inject, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+  filters: {
+    formatCardNumber(value) {
+      return value ? value.match(/.{1,4}/g).join(' ') : '';
+    },
+  },
+})
 export default class Paiement extends Vue {
+  public cardNumber = '';
+  public cardName = '';
+  public cardMonth = '';
+  public cardYear = '';
+  public cardCvv = '';
+
+  public minCardMonth = new Date().getMonth();
+  public minCardYear = new Date().getFullYear();
+
   public validerPaiement() {
     this.$router.push({ name: 'Confirmation' });
+  }
+
+  public formatCardNumber(value) {
+    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+    var matches = v.match(/\d{4,16}/g);
+    var match = (matches && matches[0]) || '';
+    var parts = [];
+
+    for (var i = 0; i < match.length; i += 4) {
+      parts.push(match.substring(i, i + 4));
+    }
+
+    if (parts.length) {
+      return parts.join(' ');
+    } else {
+      return value;
+    }
   }
 }
